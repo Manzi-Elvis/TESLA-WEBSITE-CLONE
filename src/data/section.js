b10a1ab66b8246a4fc36c1850da0fca888ceb3d9
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-15 20:42:35 +0200

    ...

---
+++ src/data/section.js
@@ -0,0 +1,17 @@
+// data/sections.js
+export const sections = [
+  {
+    title: "Model E",
+    subtitle: "Electric performance, redesigned for the future.",
+    image: "/cars/model-e.jpg",
+    primaryBtn: "Order Now",
+    secondaryBtn: "Learn More",
+  },
+  {
+    title: "Solar Roof",
+    subtitle: "Clean energy for your home.",
+    image: "/solar/solar-roof.jpg",
+    primaryBtn: "Get Started",
+    secondaryBtn: "Explore",
+  },
+];
