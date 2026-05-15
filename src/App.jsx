9a1cf97146ee2db5c37b1c1e47f21d034bc4537f
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-15 21:06:51 +0200

    imported Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar'
 function App() {
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
