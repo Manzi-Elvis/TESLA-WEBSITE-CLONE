7ce4515ec95c55e11b7d61201b76d370ddcf08fa
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-16 10:34:57 +0200

    fixed importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar'
+// import { useState } from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
