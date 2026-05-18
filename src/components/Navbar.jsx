4636509dd474647828b0fe58b5df89124208cebf
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-18 11:38:08 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import logo from '../assets/logo.png'
-import {CircleQuestionMark, Globe, CircleUserRound} from 'lucide-react'
+import logo from '../assets/logo.png';
+import {CircleQuestionMark, Globe, CircleUserRound} from 'lucide-react';
 
 export default function Navbar(){
       return(
-       <nav className="flex items-center justify-between px-8 py-0 ">
-            <img src={logo} alt="tesla logo" className="w-64 h-32 object-contain" />
-            <ul className='flex gap-8 cursor-pointer border-solid rounded-18'>
+       <nav className="h-14 flex items-center justify-between px-10 py-0">
+            <img src={logo} alt="tesla logo" className="w-28 object-contain" />
+            <ul className='flex items-center font-semibold gap-8 text-sm cursor-pointer'>
                   <li>Vehicles</li>
                   <li>Energy</li>
                   <li>Charging</li>
                   <li>Discover</li>
                   <li>Shop</li>
             </ul>
-            <div className='rightSection flex gap-4'>
-                  <CircleQuestionMark className='cursor-pointer' />
-                  <Globe className='cursor-pointer' />
-                  <CircleUserRound className='cursor-pointer' />
+            <div className='flex items-center gap-4'>
+                  <CircleQuestionMark size={26} className='cursor-pointer' />
+                  <Globe size={26} className='cursor-pointer' />
+                  <CircleUserRound size={26} className='cursor-pointer' />
             </div>
       </nav>
       )
